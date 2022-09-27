71ca96a10ca295ce8394fc3c0140f998bf3e4df5
Aravinth M <writetoaravinthmuthaiah@gmail.com>
2022-09-27 17:47:03 +0530

    second commit

---
+++ src/Class1.java
@@ -0,0 +1,12 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+System.out.println("Line 1");
+System.out.println("Line 2");
+System.out.println("Line 3");
+System.out.println("Line 4");
+System.out.println("Line 5");
+	}
+
+}
